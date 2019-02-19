2effef0b05fa91c0c9bc313ab8ec3dca077e8f57
Josh5k <jking57@academic.rrc.ca>
2019-02-19 13:46:01 -0600

    Added required pages structures

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom'
 import './navbar.scss';
 import logo from '../../assets/Logo.png'
 import logo2 from '../../assets/Logo_with_text.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
 class Navbar extends Component {
     render() {
@@ -12,13 +14,13 @@ class Navbar extends Component {
                 <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
                     <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
                         <li className="nav-item active">
-                        <Link className="nav-link" to="/">Home</Link>
+                            <Link className="nav-link" to="/">Home</Link>
                         </li>
                         <li className="nav-item">
-                        <Link className="nav-link" to="/Projects">Store</Link>
+                            <Link className="nav-link" to="/store">Store</Link>
                         </li>
                         <li className="nav-item">
-                        <Link className="nav-link" to="/developers">Management</Link>
+                            <Link className="nav-link" to="/management">Management</Link>
                         </li>
                     </ul>
                 </div>
@@ -28,19 +30,19 @@ class Navbar extends Component {
                 <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
                     <ul className="navbar-nav ml-auto">
                         <li className="nav-item">
-                            <Link className="nav-link" to="/store">Store</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="#">Contact Us</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="#">Settings</Link>
-                        </li>
-                        <li className="nav-item">
                             <form className="form-inline my-2 my-lg-0 searchbar">
                                 <input className="form-control mr-sm-2" type="search" placeholder="Search"/>
                                 <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
                             </form>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/dashboard">Dashboard</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/contact">Contact Us</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/cart"><FontAwesomeIcon icon={faShoppingCart} /></Link>
                         </li>
                     </ul>
                 </div>
